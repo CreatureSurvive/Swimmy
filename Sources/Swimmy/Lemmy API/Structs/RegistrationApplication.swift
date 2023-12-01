bab2a617e38f1e6e7b6f8f786b730f89a7564da2
CreatureSurvive <cs@creaturecoding.com>
2023-12-01 09:55:58 -0600

    Fixed missing optional value in RegistrationApplication

--- Sources/Swimmy/Lemmy API/Structs/RegistrationApplication.swift
+++ Sources/Swimmy/Lemmy API/Structs/RegistrationApplication.swift
@@ -10,12 +10,12 @@ import Foundation
 public struct RegistrationApplication: Codable, Identifiable, Hashable {
     public let admin_id: Int?
     public let answer: String
-    public let deny_reason: String
+    public let deny_reason: String?
     public let id: Int
     public let local_user_id: Int
     public let published: String
 
-    public init(admin_id: Int? = nil, answer: String, deny_reason: String, id: Int, local_user_id: Int, published: String) {
+    public init(admin_id: Int? = nil, answer: String, deny_reason: String? = nil, id: Int, local_user_id: Int, published: String) {
         self.admin_id = admin_id
         self.answer = answer
         self.deny_reason = deny_reason
